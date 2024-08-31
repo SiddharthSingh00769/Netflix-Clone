36da6762269bd01d100eb7a35dcad83b793fe177
Siddharth Singh <sid018singh@gmail.com>
2024-08-31 22:51:50 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'netflix': ['Netflix Sans','Helvetica Neue','Segoe UI','Roboto','Ubuntu','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
